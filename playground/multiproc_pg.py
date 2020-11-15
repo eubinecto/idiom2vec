609fa51ae1c2b0d89e9d3182066144b9ac1b3bbd
Eu-Bin KIM <tlrndk123@gmail.com>
2020-11-15 20:11:31 +0900

    parsing large file with multiprocessing. (parse_splits.py)

--- playground/multiproc_pg.py
+++ playground/multiproc_pg.py
@@ -1,18 +1,22 @@
 # This is a playground to get familiar with multiprocessing jobs
 # code example  from: https://docs.python.org/3/library/multiprocessing.html
 import multiprocessing as mp
+import logging
+from sys import stdout
+logging.basicConfig(stream=stdout, level=logging.INFO)
 
 
 def func(x):
-    return x * x
+    # the order in which this is computed is (should be?) arbitrary
+    print(x)
 
 
 def main():
-    with mp.Pool(processes=3) as p:
-        batches = [1, 2, 3]
+    with mp.Pool(processes=5) as p:
+        batches = list(range(1000))
         # just provide batches to the function.
         # pool.map will apply function to each batch asynchronously.
-        print(p.map(func, batches))
+        p.map(func, batches)
 
 
 if __name__ == '__main__':
